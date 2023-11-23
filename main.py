5c2c943a63206d9d4f8cd5fcb2e7486f83e96d46
Sap-Artem <75568642+Sap-Artem@users.noreply.github.com>
2023-11-23 23:13:01 +0300

    Add files via upload
    1. Добавлено разветвление после совокупности подменю, с заделом под БД и возможностью вернуться назад
    2. Подкорректированы id сообщений для оптимизации переадресации между подменю и диалоговым окном с БД
    3. Составлен первый демонстрационный отчёт
    --------------------------------------------------------------------------------------
    Инфо: все версии с подзаголовком "realise" будут для удобства сопровождаться демонстрационным отчётом

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from telebot import types
 
 bot = telebot.TeleBot('6767523338:AAHCT-k6OvYwOBGzikc71QgfW75A9XxOEYM')
 
+result_text = "Спасибо за использование нашего телеграм-бота!\n" \
+    "Ниже представлены наиболее подходящие pet-проекты согласно предоставленной информации"
 def main_menu():
     markup = types.InlineKeyboardMarkup()
     language = types.InlineKeyboardButton(text='Язык программирования', callback_data='language_message')
@@ -15,11 +17,11 @@ def main_menu():
 
 def language_func():
     language = types.InlineKeyboardMarkup()
-    si = types.InlineKeyboardButton(text='C; C++; C#', callback_data='si_message')
-    python = types.InlineKeyboardButton(text='Python', callback_data='python_message')
+    si = types.InlineKeyboardButton(text='C; C++; C#', callback_data='si_message-l')
+    python = types.InlineKeyboardButton(text='Python', callback_data='python_message-l')
     language.row(si, python)
-    html = types.InlineKeyboardButton(text='HTML; MySQL', callback_data='html_message')
-    java = types.InlineKeyboardButton(text='JavaScript', callback_data='java_message')
+    html = types.InlineKeyboardButton(text='HTML; MySQL', callback_data='html_message-l')
+    java = types.InlineKeyboardButton(text='JavaScript', callback_data='java_message-l')
     language.row(html, java)
     back = types.InlineKeyboardButton(text='Назад', callback_data='back_message')
     language.row(back)
@@ -27,11 +29,11 @@ def language_func():
 
 def number_func():
     number = types.InlineKeyboardMarkup()
-    one = types.InlineKeyboardButton(text='1', callback_data='one_message')
-    two = types.InlineKeyboardButton(text='2', callback_data='two_message')
+    one = types.InlineKeyboardButton(text='1', callback_data='one_message-n')
+    two = types.InlineKeyboardButton(text='2', callback_data='two_message-n')
     number.row(one, two)
-    five = types.InlineKeyboardButton(text='3-8', callback_data='five_message')
-    nine = types.InlineKeyboardButton(text='более 8', callback_data='nine_message')
+    five = types.InlineKeyboardButton(text='3-8', callback_data='five_message-n')
+    nine = types.InlineKeyboardButton(text='более 8', callback_data='nine_message-n')
     number.row(five, nine)
     back = types.InlineKeyboardButton(text='Назад', callback_data='back_message')
     number.row(back)
@@ -39,25 +41,49 @@ def number_func():
 
 def format_func():
     format = types.InlineKeyboardMarkup()
-    backend = types.InlineKeyboardButton(text='Backend-разработка', callback_data='backend_message')
-    frontend = types.InlineKeyboardButton(text='Frontend-разработка', callback_data='frontend_message')
+    backend = types.InlineKeyboardButton(text='Backend-разработка', callback_data='backend_message-f')
+    frontend = types.InlineKeyboardButton(text='Frontend-разработка', callback_data='frontend_message-f')
     format.row(backend, frontend)
-    mobile = types.InlineKeyboardButton(text='Mobile-разработка', callback_data='mobile_message')
+    mobile = types.InlineKeyboardButton(text='Mobile-разработка', callback_data='mobile_message-f')
     back = types.InlineKeyboardButton(text='Назад', callback_data='back_message')
     format.row(mobile, back)
     return format
 
 def time_func():
     time = types.InlineKeyboardMarkup()
-    week = types.InlineKeyboardButton(text='неделя', callback_data='week_message')
-    mouth = types.InlineKeyboardButton(text='месяц', callback_data='mouth_message')
+    week = types.InlineKeyboardButton(text='неделя', callback_data='week_message-t')
+    mouth = types.InlineKeyboardButton(text='месяц', callback_data='mouth_message-t')
     time.row(week, mouth)
-    six_mouth = types.InlineKeyboardButton(text='полгода', callback_data='six_mouth_message')
-    twelve_mouth = types.InlineKeyboardButton(text='более полугода', callback_data='twelve_mouth_message')
+    six_mouth = types.InlineKeyboardButton(text='полгода', callback_data='six_mouth_message-t')
+    twelve_mouth = types.InlineKeyboardButton(text='более полугода', callback_data='twelve_mouth_message-t')
     time.row(six_mouth, twelve_mouth)
     back = types.InlineKeyboardButton(text='Назад', callback_data='back_message')
     time.row(back)
     return time
+
+def db_language_func():
+    db_language = types.InlineKeyboardMarkup()
+    back = types.InlineKeyboardButton(text='В главное меню', callback_data='back_message')
+    db_language.row(back)
+    return db_language
+
+def db_number_func():
+    db_number = types.InlineKeyboardMarkup()
+    back = types.InlineKeyboardButton(text='В главное меню', callback_data='back_message')
+    db_number.row(back)
+    return db_number
+
+def db_format_func():
+    db_format = types.InlineKeyboardMarkup()
+    back = types.InlineKeyboardButton(text='В главное меню', callback_data='back_message')
+    db_format.row(back)
+    return db_format
+
+def db_time_func():
+    db_time = types.InlineKeyboardMarkup()
+    back = types.InlineKeyboardButton(text='В главное меню', callback_data='back_message')
+    db_time.row(back)
+    return db_time
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -77,6 +103,14 @@ def call_query(call):
             bot.send_message(call.message.chat.id, "Выбирете подходящий формат разработки", reply_markup=format_func())
         elif call.data == 'time_message':
             bot.send_message(call.message.chat.id, "Выбирете подходящие вам сроки проекта", reply_markup=time_func())
+        elif call.data[len(call.data)-1] == 'l':
+            bot.send_message(call.message.chat.id, result_text, reply_markup=db_language_func())
+        elif call.data[len(call.data)-1] == 'n':
+            bot.send_message(call.message.chat.id, result_text, reply_markup=db_number_func())
+        elif call.data[len(call.data)-1] == 'f':
+            bot.send_message(call.message.chat.id, result_text, reply_markup=db_format_func())
+        elif call.data[len(call.data)-1] == 't':
+            bot.send_message(call.message.chat.id, result_text, reply_markup=db_time_func())
         elif call.data == 'back_message':
             first_mess = f"{call.from_user.first_name} {call.from_user.last_name}, здраствуйте,\n" \
                          f"В нашем телеграм-боте вы сможете найти подходящие вам идеи pet-проектов. Вне зависимости от вашего уровня программирования, вы точно не уйдёте с пустыми руками\n" \
@@ -84,6 +118,7 @@ def call_query(call):
             bot.send_message(call.message.chat.id, first_mess, reply_markup=main_menu())
         else:
             bot.send_message(call.message.chat.id, "раздел в разработке")
+    #bot.delete_message(call.message.chat.id, call.message.message_id)
     bot.answer_callback_query(callback_query_id=call.id, show_alert=False)
 
 bot.polling()
