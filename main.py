dc8525a29774d56b25d737e2127650bfa846dfab
Sap-Artem <75568642+Sap-Artem@users.noreply.github.com>
2023-12-04 00:24:41 +0300

    Add files via upload
    1. Добавлен метод get_all для БД
    2. Добавлена новая кнопка, позволяющая вывести все pet-проекты
    3. Новый экран порционально выводящий pet-проекты
    4. Разработан задел для перемещения между блоками pet-проектов

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import telebot
 from telebot import types
-import sqlite3
+import datetime
 
-bot = telebot.TeleBot('')
+from db import database
+
+bot = telebot.TeleBot('6767523338:AAHCT-k6OvYwOBGzikc71QgfW75A9XxOEYM')
 
 result_text = "Спасибо за использование нашего телеграм-бота!\n" \
-    "Ниже представлены наиболее подходящие pet-проекты согласно предоставленной информации"
+              "Ниже представлены наиболее подходящие pet-проекты согласно предоставленной информации"
+
+
 def main_menu():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     language = types.KeyboardButton(text='Язык программирования')
@@ -14,7 +18,10 @@ def main_menu():
     format = types.InlineKeyboardButton(text='Формат проекта')
     time = types.InlineKeyboardButton(text='Сроки проекта')
     markup.add(format, time)
+    list_all = types.InlineKeyboardButton(text='Показать все pet-проекты')
+    markup.add(list_all)
     return markup
+
 
 def language_func():
     language = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -28,6 +35,7 @@ def language_func():
     language.add(back)
     return language
 
+
 def number_func():
     number = types.ReplyKeyboardMarkup()
     one = types.KeyboardButton(text='1 человек')
@@ -40,6 +48,7 @@ def number_func():
     number.row(back)
     return number
 
+
 def format_func():
     format = types.ReplyKeyboardMarkup()
     backend = types.KeyboardButton(text='направление: Backend-разработка')
@@ -49,6 +58,16 @@ def format_func():
     back = types.KeyboardButton(text='Назад')
     format.row(mobile, back)
     return format
+
+
+def format_day(num):
+    if num == 1:
+        return f"{num} день"
+    elif num in (2, 3, 4):
+        return f"{num} дня"
+    else:
+        return f"{num} дней"
+
 
 def time_func():
     time = types.ReplyKeyboardMarkup()
@@ -62,79 +81,35 @@ def time_func():
     time.row(back)
     return time
 
+
 def database_language(message):
-    sqlite_connection = sqlite3.connect('dbtest.db')
-    cursor = sqlite_connection.cursor()
-    sqlite_select_query = """SELECT * from idea"""
-    cursor.execute(sqlite_select_query)
-    records = cursor.fetchall()
-    list = []
-    for row in records:
-        if str(row[8])[len(str(row[8]))-6:len(str(row[8]))] == str(message)[len(str(message))-6:len(str(message))]:
-            list.append(row[0])
-            list.append(row[1])
-            list.append(row[4])
-    return list
+    message = message.replace(' ', '|', 1)
+    db = database.BotDataBase('db/database.db')
+    answer = db.search_by_language(message.split('|')[1].split('; '))
+    return answer
+
+
 def database_number(message):
-    sqlite_connection = sqlite3.connect('dbtest.db')
-    cursor = sqlite_connection.cursor()
-    sqlite_select_query = """SELECT * from idea"""
-    cursor.execute(sqlite_select_query)
-    records = cursor.fetchall()
-    list = []
-    for row in records:
-        k = False
-        if message == "1 человек":
-            k = (row[6] == 1)
-        elif message == "2 человека":
-            k = (row[6] == 2)
-        elif message == "3-8 человек":
-            k = (row[6] >= 3 & row[6] <= 8)
-        elif message == "более 8 человек":
-            k = (row[6] > 8)
-        if k:
-            list.append(row[0])
-            list.append(row[1])
-            list.append(row[4])
-    return list
+    db = database.BotDataBase('db/database.db')
+    answer = db.search_by_people(message)
+    return answer
+
 
 def database_format(message):
-    sqlite_connection = sqlite3.connect('dbtest.db')
-    cursor = sqlite_connection.cursor()
-    sqlite_select_query = """SELECT * from idea"""
-    cursor.execute(sqlite_select_query)
-    records = cursor.fetchall()
-    list = []
-    for row in records:
-        if str(row[5])[len(str(row[5])) - 17:len(str(row[5]))] == str(message)[len(str(message)) - 17:len(str(message))]:
-            list.append(row[0])
-            list.append(row[1])
-            list.append(row[4])
-    return list
+    db = database.BotDataBase('db/database.db')
+    answer = db.search_by_format(message[13:])
+    return answer
+
 
 def database_time(message):
-    sqlite_connection = sqlite3.connect('dbtest.db')
-    cursor = sqlite_connection.cursor()
-    sqlite_select_query = """SELECT * from idea"""
-    cursor.execute(sqlite_select_query)
-    records = cursor.fetchall()
-    list = []
-    for row in records:
-        value = int(str(row[7])[0:str(row[7]).find(" ")])
-        k = False
-        if message == "срок: меньше недели":
-            k = (value < 7)
-        elif message == "срок: от недели до месяца":
-            k = (value >= 7) & (value <= 31)
-        elif message == "срок: от месяца до полугода":
-            k = ((value > 31) & (value <= 180))
-        elif message == "срок: более полугода":
-            k = (value > 180)
-        if k:
-            list.append(row[0])
-            list.append(row[1])
-            list.append(row[4])
-    return list
+    db = database.BotDataBase('db/database.db')
+    answer = db.search_by_time(message)
+    return answer
+
+def database_all():
+    db = database.BotDataBase('db/database.db')
+    answer = db.get_all()
+    return answer
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -143,27 +118,22 @@ def start(message):
                  f"Прежде чем начать, пожалуйста, выберите категорию поиска"
     bot.send_message(message.chat.id, first_mess, reply_markup=main_menu())
 
+
 @bot.callback_query_handler(func=lambda call: True)
 def call_query(call):
     if call.message:
         if call.data[0:7] == 'project':
-            sqlite_connection = sqlite3.connect('dbtest.db')
-            cursor = sqlite_connection.cursor()
-            sqlite_select_query = """SELECT * from idea"""
-            cursor.execute(sqlite_select_query)
-            records = cursor.fetchall()
-            string = ""
-            for row in records:
-                if int(call.data[call.data.find("_")+1:len(call.data)]) == int(row[0]):
-                    string = "Название проекта: " + str(row[1]) + "\n" + \
-                             "Балл: " + str(row[2]) + "\n" + \
-                             "Описание : " + str(row[3]) + "\n" + \
-                             "Направление: " + str(row[5]) + "\n" + \
-                             "Количество участников: " + str(row[6]) + "\n" + \
-                             "Сроки реализации: " + str(row[7]) + "\n" + \
-                             "Язык программирования: " + str(row[7]) + "\n" + \
-                             "Аудитория: " + str(row[9]) + "\n" + \
-                             "Предлагаемые технологии: " + str(row[10]) + "\n"
+            info_db = database.BotDataBase('db/database.db')
+            data = info_db.get_by_id(int(call.data.split('_')[1]))
+            string = "Название проекта: " + str(data[1]) + "\n" + \
+                     "Рейтинг: " + str(data[2]) + "\n" + \
+                     "Описание: " + str(data[3]) + "\n" + \
+                     "Направление: " + str(data[5]) + "\n" + \
+                     "Количество участников: " + str(data[7]) + "\n" + \
+                     "Сроки реализации: " + format_day(data[8]) + "\n" + \
+                     "Язык программирования: " + str(data[6]) + "\n" + \
+                     "Сложность: " + str(data[9]) + '/10' + "\n" + \
+                     "Предлагаемые технологии: " + str(data[11]) + "\n"
             db = types.InlineKeyboardMarkup()
             back = types.InlineKeyboardButton(text='В главное меню', callback_data='back_message')
             db.row(back)
@@ -173,6 +143,7 @@ def call_query(call):
                          f"В нашем телеграм-боте вы сможете найти подходящие вам идеи pet-проектов. Вне зависимости от вашего уровня программирования, вы точно не уйдёте с пустыми руками\n" \
                          f"Прежде чем начать, пожалуйста, выберите категорию поиска"
             bot.send_message(call.message.chat.id, first_mess, reply_markup=main_menu())
+
 
 @bot.message_handler(content_types=['text'])
 def call_message(message):
@@ -184,34 +155,49 @@ def call_message(message):
         bot.send_message(message.chat.id, "Выбирете подходящий формат разработки", reply_markup=format_func())
     elif message.text == 'Сроки проекта':
         bot.send_message(message.chat.id, "Выбирете подходящие вам сроки проекта", reply_markup=time_func())
+    elif message.text == 'Показать все pet-проекты':
+        all_time = str(datetime.datetime.now())
+        years = all_time[0:4]
+        month = all_time[5:7]
+        day = all_time[8:10]
+        time = all_time[10:19]
+        list = database_all()
+        db = types.InlineKeyboardMarkup()
+        for data in list:
+            project = types.InlineKeyboardButton(text=data[1] + " - " + data[2], callback_data='project_' + str(data[0]))
+            db.row(project)
+        left = types.InlineKeyboardButton(text='<', callback_data='left_message')
+        pos = types.InlineKeyboardButton(text='1/1', callback_data='pos_message')
+        right = types.InlineKeyboardButton(text='>', callback_data='right_message')
+        db.row(left, pos, right)
+        back = types.InlineKeyboardButton(text='В главное меню', callback_data='back_message')
+        db.row(back)
+        bot.send_message(message.chat.id, text="Все pet-проекты загруженные до " + time + " " + day + "." + month + "." + years, parse_mode='Markdown', reply_markup=db)
     elif str(message.text)[0:4] == 'язык':
-        #bot.send_message(message.chat.id, result_text + "\n" + database_language(message.text), parse_mode= 'Markdown', reply_markup=db_language_func())
+        # bot.send_message(message.chat.id, result_text + "\n" + database_language(message.text), parse_mode= 'Markdown', reply_markup=db_language_func())
         bot.send_message(message.chat.id, result_text)
         list = database_language(message.text)
-        i = 0
-        while i < len(list):
+        for data in list:
             db = types.InlineKeyboardMarkup()
-            project = types.InlineKeyboardButton(text=list[i+1], callback_data='project_'+str(list[i]))
+            project = types.InlineKeyboardButton(text=data[1], callback_data='project_' + str(data[0]))
             db.row(project)
-            bot.send_message(message.chat.id, list[i+1] + "\n" + list[i+2], parse_mode='Markdown', reply_markup=db)
-            i = i + 3
-        if len(list) == 0:
+            bot.send_message(message.chat.id, data[1] + "\n" + data[2], parse_mode='Markdown', reply_markup=db)
+        if not list:
             bot.send_message(message.chat.id, "Не найдено ни одного pet-проекта. Попробуйте изменить параметры посика.")
         db_language = types.InlineKeyboardMarkup()
         back = types.InlineKeyboardButton(text='В главное меню', callback_data='back_message')
         db_language.row(back)
         bot.send_message(message.chat.id, text="Или вернитесь в главное меню", reply_markup=db_language)
-    elif (str(message.text)[len(str(message.text))-7:len(str(message.text))] == 'человек') | (str(message.text)[len(str(message.text))-8:len(str(message.text))] == 'человека'):
+    elif (str(message.text)[len(str(message.text)) - 7:len(str(message.text))] == 'человек') | (
+            str(message.text)[len(str(message.text)) - 8:len(str(message.text))] == 'человека'):
         bot.send_message(message.chat.id, result_text)
         list = database_number(message.text)
-        i = 0
-        while i < len(list):
+        for data in list:
             db = types.InlineKeyboardMarkup()
-            project = types.InlineKeyboardButton(text=list[i+1], callback_data='project_'+str(list[i]))
+            project = types.InlineKeyboardButton(text=data[1], callback_data='project_' + str(data[0]))
             db.row(project)
-            bot.send_message(message.chat.id, list[i+1] + "\n" + list[i + 2], parse_mode='Markdown', reply_markup=db)
-            i = i + 3
-        if len(list) == 0:
+            bot.send_message(message.chat.id, data[1] + "\n" + data[2], parse_mode='Markdown', reply_markup=db)
+        if not list:
             bot.send_message(message.chat.id, "Не найдено ни одного pet-проекта. Попробуйте изменить параметры посика.")
         db_number = types.InlineKeyboardMarkup()
         back = types.InlineKeyboardButton(text='В главное меню', callback_data='back_message')
@@ -220,14 +206,12 @@ def call_message(message):
     elif str(message.text)[0:11] == 'направление':
         bot.send_message(message.chat.id, result_text)
         list = database_format(message.text)
-        i = 0
-        while i < len(list):
+        for data in list:
             db = types.InlineKeyboardMarkup()
-            project = types.InlineKeyboardButton(text=list[i+1], callback_data='project_'+str(list[i]))
+            project = types.InlineKeyboardButton(text=data[1], callback_data='project_' + str(data[0]))
             db.row(project)
-            bot.send_message(message.chat.id, list[i+1] + "\n" + list[i + 2], parse_mode='Markdown', reply_markup=db)
-            i = i + 3
-        if len(list) == 0:
+            bot.send_message(message.chat.id, data[1] + "\n" + data[2], parse_mode='Markdown', reply_markup=db)
+        if not list:
             bot.send_message(message.chat.id, "Не найдено ни одного pet-проекта. Попробуйте изменить параметры посика.")
         db_format = types.InlineKeyboardMarkup()
         back = types.InlineKeyboardButton(text='В главное меню', callback_data='back_message')
@@ -236,27 +220,26 @@ def call_message(message):
     elif str(message.text)[0:4] == 'срок':
         bot.send_message(message.chat.id, result_text)
         list = database_time(message.text)
-        i = 0
-        while i < len(list):
+        for data in list:
             db = types.InlineKeyboardMarkup()
-            project = types.InlineKeyboardButton(text=list[i+1], callback_data='project_'+str(list[i]))
+            project = types.InlineKeyboardButton(text=data[1], callback_data='project_' + str(data[0]))
             db.row(project)
-            bot.send_message(message.chat.id, list[i+1] + "\n" + list[i + 2], parse_mode='Markdown', reply_markup=db)
-            i = i + 3
-        if len(list) == 0:
+            bot.send_message(message.chat.id, data[1] + "\n" + data[2], parse_mode='Markdown', reply_markup=db)
+        if not list:
             bot.send_message(message.chat.id, "Не найдено ни одного pet-проекта. Попробуйте изменить параметры посика.")
         db_time = types.InlineKeyboardMarkup()
         back = types.InlineKeyboardButton(text='В главное меню', callback_data='back_message')
         db_time.row(back)
         bot.send_message(message.chat.id, text="Или вернитесь в главное меню", reply_markup=db_time)
     elif message.text == 'Назад':
-        first_mess = f"{message.from_user.first_name} {message  .from_user.last_name}, здраствуйте,\n" \
-                         f"В нашем телеграм-боте вы сможете найти подходящие вам идеи pet-проектов. Вне зависимости от вашего уровня программирования, вы точно не уйдёте с пустыми руками\n" \
-                         f"Прежде чем начать, пожалуйста, выберите категорию поиска"
+        first_mess = f"{message.from_user.first_name} {message.from_user.last_name}, здраствуйте,\n" \
+                     f"В нашем телеграм-боте вы сможете найти подходящие вам идеи pet-проектов. Вне зависимости от вашего уровня программирования, вы точно не уйдёте с пустыми руками\n" \
+                     f"Прежде чем начать, пожалуйста, выберите категорию поиска"
         bot.send_message(message.chat.id, first_mess, reply_markup=main_menu())
     else:
         bot.send_message(message.chat.id, "раздел в разработке")
-    #bot.delete_message(call.message.chat.id, call.message.message_id)
-    #bot.answer_callback_query(callback_query_id=message.id, show_alert=False)
+    # bot.delete_message(message.chat.id, message.message_id)
+    # bot.answer_callback_query(callback_query_id=message.id, show_alert=False)
+
 
 bot.polling()
