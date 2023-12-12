efeeb470ea2cb6cc9e3a1f9e4fa28c030f98b4e1
Alex00999 <qx77525@gmail.com>
2023-12-13 02:50:32 +0300

    Обновил структуру под новую таблицу. Исправил ошибки

--- db/database.py
+++ db/database.py
@@ -41,17 +41,20 @@ class BotDataBase:
         self.cursor.execute(query)
         self.db.commit()
 
-    def search_by_language(self, languages: list or str):
+    def search_by_language(self, lang_str):
+        print(lang_str)
         """
         Метод принимает язык программирования и выводит краткую информацию о всех идеях с этим языком программирования из БД.
-        :param languages:
+        :param lang_str:
         :return: Список из id, name, summary каждой идеи.
         """
+        if ';' in lang_str:
+            languages = lang_str.split('; ')
+        else:
+            languages = [lang_str]
         response = []
-        if languages is not list:
-            languages = [languages]
         for language in languages:
-            query = f''' SELECT ideas.id, ideas.name, ideas.summary FROM ideas JOIN languages ON languages.language_id = ideas.language_id WHERE language_name="{language}";  '''
+            query = f''' SELECT ideas.id, ideas.name, ideas.summary, language_name FROM ideas JOIN languages ON languages.language_id = ideas.language_id WHERE language_name="{language}";  '''
             self.cursor.execute(query)
             response += [res for res in self.cursor]
         return response
@@ -112,12 +115,15 @@ class BotDataBase:
         :param idea_id: ID
         :return: id, name, rating, description, summary, theme, language, level, technologies_id, technologies_name
         """
-        query = f''' SELECT * FROM IDEAS WHERE IDEAS.id={idea_id}; '''
+        query = f''' 
+            SELECT ideas.name, ideas.rating, ideas.description, themes.theme_name, ideas.people, ideas.time, languages.language_name, ideas.level, technologies.name FROM IDEAS
+            INNER JOIN languages ON languages.language_id = ideas.language_id
+            INNER JOIN themes ON themes.id = ideas.theme_id
+            INNER JOIN technologies ON technologies.id = ideas.technologies_id
+            WHERE IDEAS.id={idea_id}
+            '''
         self.cursor.execute(query)
         response = self.cursor.fetchone()
-        query = f''' SELECT TECHNOLOGIES.NAME FROM TECHNOLOGIES WHERE TECHNOLOGIES.id={response[10]};'''
-        self.cursor.execute(query)
-        response += (self.cursor.fetchone())
         return response
 
     def get_all(self, n=None, reverse=False):
@@ -150,15 +156,56 @@ class BotDataBase:
         response = self.cursor.fetchall()
         return check_id in [item[0] for item in response]
 
+    def get_language_id(self, item):
+        query = f''' SELECT language_id FROM languages WHERE language_name="{item}"; '''
+        self.cursor.execute(query)
+        response = self.cursor.fetchone()
+        if response:
+            return response[0]
+        return 0
 
-# db = BotDataBase('database.db')
-# print(db.is_admin(1234562))
-# db.add('Тест 1', 11, 'Описание идеи', 'Краткое описание', 'Backend-разработка', 1, 3, 1)
-# db.add('Классная идея 2', 8, 'Описание идеи 2', 'Краткое описание 2',
-# 'Mobile-разработка', 'JavaScript', 3, 2)
-# db.add('Классная идея 3', 7, 'Описание идеи 3', 'Краткое описание 1',
-# 'Mobile-разработка', 'Python', 3, 3)
-# print(db.search_by_language('Python'))
-# print(db.search_by_people('более 8 человек'))
-# print(db.search_by_format('Frontend-разработка'))
-# print(db.search_by_time('срок: меньше недели'))
+    def get_theme_id(self, item):
+        item = item.split('-')[0]
+        query = f''' SELECT id FROM themes WHERE theme_name="{item}"; '''
+        self.cursor.execute(query)
+        response = self.cursor.fetchone()
+        if response:
+            return response[0]
+        return 0
+
+    def add_suggestion(self, name='', rating=0, description='', summary='', theme='', language='', people=0, time=0,
+                       level=0,
+                       technologies=''):
+        language = self.get_language_id(language)
+        theme = self.get_theme_id(theme)
+        query = """ INSERT INTO suggestions
+        (NAME, RATING, DESCRIPTION, SUMMARY, theme_id, LANGUAGE_ID, PEOPLE, TIME, LEVEL, TECHNOLOGIES_ID)
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?,?) """
+        self.cursor.execute(query,
+                            (name, rating, description, summary, theme, language, people, time, level, technologies))
+        self.db.commit()
+
+    def ideas_amount(self) -> int:
+        """
+        Метод возвращает количество идей в БД.
+        :return: Число идей в БД
+        """
+        query = ''' SELECT COUNT(*) FROM IDEAS; '''
+        self.cursor.execute(query)
+        return self.cursor.fetchone()[0]
+
+if __name__ == "__main__":
+    db = BotDataBase('database.db')
+    # print(db.is_admin(1234562))
+    print(db.search_by_language('Python'))
+    print(db.search_by_language('C#; C++'))
+    # print(db.get_by_id(3))
+    # db.add_suggestion('Тест 1', 11, 'Описание идеи', 'Краткое описание', 'Backend-разработка', 'Python', 3, 1)
+    # db.add('Классная идея 2', 8, 'Описание идеи 2', 'Краткое описание 2',
+    # 'Mobile-разработка', 'JavaScript', 3, 2)
+    # db.add('Классная идея 3', 7, 'Описание идеи 3', 'Краткое описание 1',
+    # 'Mobile-разработка', 'Python', 3, 3)
+    # print(db.search_by_language('Python'))
+    # print(db.search_by_people('более 8 человек'))
+    # print(db.search_by_format('Frontend-разработка'))
+    # print(db.search_by_time('срок: меньше недели'))
